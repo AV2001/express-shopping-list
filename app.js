b1233dd4b94a61a29dc8b7d9ff4de8c8b8d14fa0
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-01 15:16:33 +0400

    use item routes and create error middleware

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const itemRoutes = require('./routes/itemRoutes');
+const ExpressError = require('./expressError');
 
 const app = express();
 
 app.use(express.json());
+app.use('/items', itemRoutes);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message;
+    return res.status(message).json({
+        error: { message, status },
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running on port 3000.');

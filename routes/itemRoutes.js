8887c86e1f6e5451bf96d809055548a5cc3ca8b7
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-01 15:20:05 +0400

    add route to add new item

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 const items = require('../fakeDb');
 
+// Route to get all items
 router.get('/', (req, res) => {
     return res.status(200).json({ items });
+});
+
+// Route to add new item
+router.post('/', (req, res) => {
+    const newItem = req.body;
+    items.push(newItem);
+    return res
+        .status(201)
+        .json({ added: { name: newItem.name, price: newItem.price } });
 });
 
 module.exports = router;

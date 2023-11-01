b2d3188cea8a6dd9b869df350e14b05bfe7f76f5
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-01 15:33:00 +0400

    add route to request a particular item

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const items = require('../fakeDb');
+const ExpressError = require('../expressError');
 
 // Route to get all items
 router.get('/', (req, res) => {
@@ -14,6 +15,14 @@ router.post('/', (req, res) => {
     return res
         .status(201)
         .json({ added: { name: newItem.name, price: newItem.price } });
+});
+
+// Route to get a particular item based on name
+router.get('/:name', (req, res) => {
+    const { name } = req.params;
+    const foundItem = items.find((item) => item.name === name);
+    if (!foundItem) throw new ExpressError('Item not found.', 404);
+    return res.status(200).json(foundItem);
 });
 
 module.exports = router;

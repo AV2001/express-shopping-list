c96f5eb35ada2179b6541a7aab3e7f25126596a9
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-02 19:59:25 +0400

    change name from itemRoutes to items when importing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const itemRoutes = require('./routes/itemRoutes');
+const itemRoutes = require('./routes/items');
 const ExpressError = require('./expressError');
 
 app.use(express.json());

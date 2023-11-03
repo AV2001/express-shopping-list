ad43d8e89bef1c96e1ea7ccce7e1771f09d78459
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-11-03 11:13:17 +0400

    add tests for POST /items

--- routes/items.test.js
+++ routes/items.test.js
@@ -14,9 +14,35 @@ afterEach(() => {
 });
 
 describe('GET /items', () => {
-    test('', async () => {
+    test('it should return all items', async () => {
         const response = await request(app).get('/items');
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual([item]);
+    });
+});
+
+describe('POST /items', () => {
+    test('it should add an item', async () => {
+        const newItem = { name: 'cheerios', price: 3.4 };
+        const response = await request(app).post('/items').send(newItem);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual({ added: newItem });
+    });
+
+    test('it should response with 400 if missing name and price', async () => {
+        const response = await request(app).post('/items');
+        expect(response.statusCode).toBe(400);
+    });
+
+    test('it should respond with 400 if missing name', async () => {
+        const newItem = { price: 3.4 };
+        const response = await request(app).post('/items').send(newItem);
+        expect(response.statusCode).toBe(400);
+    });
+
+    test('it should respond with 400 if missing price', async () => {
+        const newItem = { name: 'cheerios' };
+        const response = await request(app).post('/items').send(newItem);
+        expect(response.statusCode).toBe(400);
     });
 });
